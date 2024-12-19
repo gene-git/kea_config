a48ff460455e46c7f04f91a5412e03f8b6c814c4
Gene C <arch@sapience.com>
2024-12-19 11:03:03 -0500

    add new file toml.py

---
+++ src/kea_config/kea/toml.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2022-present Gene C
+''' toml support'''
+import os
+import tomllib as toml
+
+def read_toml_file(fpath):
+    """
+    read toml file and return a dictionary
+    """
+    this_dict = None
+    if os.path.exists(fpath):
+        with open(fpath, 'r', encoding='utf=8') as fob:
+            data = fob.read()
+            this_dict = toml.loads(data)
+    return this_dict
