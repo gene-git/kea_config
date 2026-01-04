900b8570f23f542987ca9e8e27bf4624da8e8c1f
Gene C <arch@sapience.com>
2026-01-04 14:30:48 -0500

    Update arch PKGBUILD

--- Docs/conf.py
+++ Docs/conf.py
@@ -9,7 +9,7 @@
 project = "kea_config"
 copyright = '2023, Gene C'
 author = 'Gene C'
-release = "5.2.0"
+release = "5.2.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- src/kea_config_mod/kea/version.py
+++ src/kea_config_mod/kea/version.py
@@ -3,4 +3,4 @@
 """
 Project kea-config
 """
-__version__ = "5.2.0"
+__version__ = "5.2.1"
